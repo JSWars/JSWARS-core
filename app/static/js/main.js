958cffd3f673fb2e3ce5f7974ddfd599fa39911b
MarcX <djwedo@gmail.com>
2014-11-20 22:01:38 +0100

    Adding material design and rquirejs. Hammer shim not working.

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,15 +1,52 @@
+"use strict";
+/**/
 // contents of main.js:
 require.config({
-    "baseUrl": "../",
-    "paths": {
-        "angular": "static/components/angular/angular.min",
-        "angular-material": "static/components/angular-material/angular-material",
-
+    'baseUrl': './',
+    'paths': {
+        'domReady': 'components/domReady/domReady',
+        'angular': 'components/angular/angular',
+        'angular-material': 'components/angular-material/angular-material',
+        'angular-animate': 'components/angular-animate/angular-animate',
+        'angular-aria': 'components/angular-aria/angular-aria',
+        'hammer': 'components/hammerjs/hammer',
+        'angular-ui-router': 'components/angular-ui-router/release/angular-ui-router'
+    },
+    'shim': {
+        'angular': {
+            'exports': 'angular'
+        },
+        'angular-animate': {
+            'deps': ['angular']
+        },
+        'angular-aria': {
+            'deps': ['angular']
+        },
+        'angular-ui-router': {
+            'deps': ['angular']
+        },
+        'hammer': {
+            'exports': 'Hammer'
+        },
+        'angular-material': {
+            'deps': ['angular', 'angular-aria', 'angular-animate']
+        }
     }
 });
 
 require([
-    'angular',
-], function () {
+    'domReady',
+    'hammer',
+    'angular-material',
+], function (domReady) {
+
+    var tfgApp = angular.module('tfg', ['ngMaterial']);
+    tfgApp.controller('LoginController', function ($scope) {
+    });
+
+    domReady(function () {
+        angular.bootstrap(document.documentElement, ['tfg']);
+    });
+
 
 });
