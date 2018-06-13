a9ac93de927b965a435598d8414f9f548989e54f
Marcos Pérez Ferro <djwedo@gmail.com>
2018-06-13 13:38:27 +0200

    Deleting not working tests.

--- test/unit.run.js
+++
@@ -1,8 +0,0 @@
-var Jasmine = require('jasmine');
-var jasmine = new Jasmine();
-
-jasmine.loadConfigFile('test/jasmine.json');
-jasmine.configureDefaultReporter({
-	showColors: true
-});
-jasmine.execute();
